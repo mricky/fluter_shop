8ee7735a01a339206645ec03209a8136fefa87c2
Mohammad <mricky.it@gmail.com>
2019-12-25 23:09:19 +0700

    Multi Provider - Connectiong the Cart Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/providers/cart.dart';
 //import 'package:flutter_shop/models/product.dart';
 import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
-
+import './providers/cart.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-        value: Products(),
+    return MultiProvider(providers: [
+          ChangeNotifierProvider.value(
+            value: Products(),
+          ),
+          ChangeNotifierProvider.value(
+            value: Cart(),
+          ),
+        ],
         child: MaterialApp(
         title: 'My Shop',
         theme: ThemeData(
