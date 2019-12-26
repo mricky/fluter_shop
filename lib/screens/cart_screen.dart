5cd0706aecdd836b269cd7da05b143e1f414862d
Mohammad <mricky.it@gmail.com>
2019-12-26 11:34:24 +0700

    Making Orders Expandable & Statefull Widget & Providers

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -27,7 +27,7 @@ class CartScreen extends StatelessWidget {
                Spacer(),
                Chip(
                  label: 
-                 Text('\$${cart.totalAmount}', 
+                 Text('\$${cart.totalAmount.toStringAsFixed(2)}', 
                         style: TextStyle(color: Theme.of(context).primaryTextTheme.title.color),),
                  backgroundColor: Theme.of(context).primaryColor,
               
