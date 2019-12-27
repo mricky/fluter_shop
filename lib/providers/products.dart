0cb5ac19c23a130d2f763b4d1613f26a5ebc9eae
Mohammad <mricky.it@gmail.com>
2019-12-27 13:28:27 +0700

    Sending Post Request

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import './product.dart';
+import 'package:http/http.dart' as http;
 
 class Products with ChangeNotifier {
   final List<Product> _items = [
@@ -60,16 +62,25 @@ class Products with ChangeNotifier {
         return _items.firstWhere((prod) => prod.id == id);
   }
   void addProducts(Product product){
-  final newProduct = Product(
-    title: product.title,
-    description: product.description, 
-    price: product.price,
-    imageUrl: product.imageUrl,
-    id: DateTime.now().toString(),
-    );
-    _items.add(newProduct);
-   // _items.insert(0, newProduct); // at the start of the list
-    notifyListeners();
+    const url = 'https://flutter-shop-37221.firebaseio.com/products.json';
+    http.post(url,body: json.encode({
+      'title': product.title,
+      'description': product.description,
+      'imageUrl': product.imageUrl,
+      'price': product.price,
+      'isFavorite': product.isFavorite
+    }),);
+
+    final newProduct = Product(
+      title: product.title,
+      description: product.description, 
+      price: product.price,
+      imageUrl: product.imageUrl,
+      id: DateTime.now().toString(),
+      );
+      _items.add(newProduct);
+    // _items.insert(0, newProduct); // at the start of the list
+      notifyListeners();
   }
   void updatProduct(String id, Product newProduct){
     final productIndex = _items.indexWhere((prod) => prod.id == id);
